ed2cf9bed2f40e6239f0de2defc66cde213d2445
Jakub Jirutka <jakub@jirutka.cz>
2022-10-09 21:32:23 +0200

    Fix 'cargo test' with default features disabled

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,46 @@
+#![deny(missing_docs)]
+#![deny(warnings)]
 //! # urlshortener
 //!
 //! An easy library for retrieving short urls.
 //!
-//! ## Usage
-//!
-//! Creating a short URL via a specified provider is very simple:
-//!
-//! ```rust,no_run
-//! use urlshortener::{providers::Provider, client::UrlShortener};
-//!
-//! let us = UrlShortener::new().unwrap();
-//! let short_url = us.generate("https://my-long-url.com", &Provider::IsGd);
-//! assert!(short_url.is_ok());
-//! ```
-//!
-//! Or attempting all URL shorteners until one is successfully generated:
-//!
-//! ```rust,no_run
-//! use urlshortener::client::UrlShortener;
-//!
-//! let us = UrlShortener::new().unwrap();
-//! let short_url = us.try_generate("https://my-long-url.com", None);
-//! assert!(short_url.is_ok());
-//! ```
-//! In order to use service with authentication use the appropriate provider directly:
-//!
-//! ```rust,no_run
-//! use urlshortener::{ client::UrlShortener, providers::Provider };
-//!
-//! let us = UrlShortener::new().unwrap();
-//! let key = "MY_API_KEY";
-//! let short_url = us.generate("https://my-long-url.com", &Provider::GooGl { api_key:
-//! key.to_owned() });
-//! assert!(short_url.is_ok());
-//! ```
-#![deny(missing_docs)]
-#![deny(warnings)]
+#[cfg_attr(
+    feature = "client",
+    doc = r##"
+## Usage
+
+Creating a short URL via a specified provider is very simple:
+
+```rust,no_run
+use urlshortener::{providers::Provider, client::UrlShortener};
+
+let us = UrlShortener::new().unwrap();
+let short_url = us.generate("https://my-long-url.com", &Provider::IsGd);
+assert!(short_url.is_ok());
+```
+
+Or attempting all URL shorteners until one is successfully generated:
+
+```rust,no_run
+use urlshortener::client::UrlShortener;
+
+let us = UrlShortener::new().unwrap();
+let short_url = us.try_generate("https://my-long-url.com", None);
+assert!(short_url.is_ok());
+```
+In order to use service with authentication use the appropriate provider directly:
+
+```rust,no_run
+use urlshortener::{ client::UrlShortener, providers::Provider };
+
+let us = UrlShortener::new().unwrap();
+let key = "MY_API_KEY";
+let short_url = us.generate("https://my-long-url.com", &Provider::GooGl { api_key:
+key.to_owned() });
+assert!(short_url.is_ok());
+```
+"##
+)]
 
 /// A urlshortener http client for performing requests.
 #[cfg(feature = "client")]
